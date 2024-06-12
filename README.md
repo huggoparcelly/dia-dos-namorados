1543f87f6dbdbe9345fa06f64edbfc26fa38b6bb
huggoparcelly <h.parcelly@gmail.com>
2024-06-12 08:40:35 -0300

    Feliz dia dos namorados

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cartinha de dia dos namorados
---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+$(document).ready(function(){
+  $('.title').click(function(){
+      $('.container').addClass('open');
+  });
+  
+  $('.close').click(function(){
+      $('.container').removeClass('open');
+  });
+});
